04c433bb5fff55fc61466f2fa08e453ad840dd8c
Rohit Jain <rohitjain991100@gmail.com>
2020-04-19 17:27:59 +0530

    modified urls

--- Components/Signin/Signin.js
+++ Components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component{
       }
 
       onButtonClickSignin=()=>{
-        fetch('https://warm-reef-38212.herokuapp.com' , {
+        fetch('https://warm-reef-38212.herokuapp.com/signin' , {
           method : 'post',
           headers : {'Content-Type' : 'application/json'},
           body:JSON.stringify({
